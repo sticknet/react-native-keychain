03979ccd6a84da6f160c66e33444b24ac33479ca
Omar Basem <founder@stiiick.com>
2022-06-08 18:32:57 +0400

    first commit

---
+++ android/build/generated/source/buildConfig/release/com/oblador/keychain/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.oblador.keychain;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.oblador.keychain";
+  public static final String BUILD_TYPE = "release";
+}
